6c2a81d11e6d9d365a6205ddc1e98a1dcb5346a0
Matthew Scorziello <scorziello.matthew@gmail.com>
2025-11-18 14:46:53 -0500

    Implement twoSum method in Solution class

---
+++ 1_Two_Sum.py
@@ -0,0 +1,9 @@
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            for t in range(n-i-1):
+                if (nums[i] + nums[t+i+1]) == target:
+                    return [i,t+i+1]
